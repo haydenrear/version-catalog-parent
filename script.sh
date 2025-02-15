89eb7b9943359dfe0dae4345f6811ab959bfa70a
hayden.rear <hayden.rear@gmail.com>
2025-02-15 16:23:38 -0500

    version catalog

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "$HOME"
+./gradlew version_catalog:build
 ./gradlew version_catalog:generateCatalogAsToml
 cp version_catalog/build/version-catalog/libs.versions.toml "$HOME"/.gradle
 cd hayden-bom || exit
 ./gradlew build
+./gradlew publishToMavenLocal
 cd ..
 cd hayden-bom-plugin/build_hayden_bom || exit
 ls
